2333774bf2d89fd80a9b033a6e92627372dc9410
yamamoto-hirofumi <sun.lionkig.88@gmail.com>
2021-07-07 02:46:34 +0000

    [3-5] finish

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,10 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
@@ -10,9 +12,9 @@ class BlogsController < ApplicationController
   end
 
   def create
-    blog = Blog.new(blog-params)
-    blog.sve
-    redirect_to blog_path
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path(blog.id)
   end
 
   def edit
